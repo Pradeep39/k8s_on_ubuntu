aa110e844f164ab08239984df17427fc622b3230
Pradeep <pradeepreddy.alla@gmail.com>
2025-01-09 10:46:36 -0600

    Create setup_containerd.sh
    Shell Script that installs containerd on ubuntu / Debian based systems

---
+++ setup_containerd.sh
@@ -0,0 +1,24 @@
+###Load kernel modules
+sudo modprobe overlay && \
+sudo modprobe br_netfilter
+
+#Setup apt-get sources
+sudo mkdir -p /etc/apt/keyrings
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+###Install containerd
+sudo apt-get update -y && \
+sudo apt-get install containerd.io
+
+###Tweak config
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+###Enable containerd and check status
+sudo systemctl enable containerd
+sudo systemctl status containerd
